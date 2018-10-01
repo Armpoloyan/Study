fa62ef612b307bb7e6f1a8249583338fba0496c6
Arman <Arman@ARMAN-PC>
2018-10-01 13:03:55 +0400

    add calc

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.Write("Please enter the First number: ");
+
+            Double FirstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter the operation type: ");
+
+            string operationType = Console.ReadLine();
+
+            Console.Write("Please enter the Second number: ");
+
+            double SecondNumber = Convert.ToDouble(Console.ReadLine());
+
+            
+
+            Double result= 0;
+
+            if (operationType == "+")
+
+            {
+                result = FirstNumber + SecondNumber;
+
+            }
+            else if (operationType == "-")
+            {
+
+                result = FirstNumber - SecondNumber;
+            }
+
+            else if (operationType == "*")
+
+            {
+
+                result = FirstNumber * SecondNumber;
+
+            }
+
+            else if (operationType == "/")
+
+            {
+
+                result = FirstNumber / SecondNumber;
+
+            }
+
+            else if (operationType == "%")
+
+            {
+
+                result = FirstNumber % SecondNumber;
+
+            }
+
+            else if (operationType == "^")
+
+            {
+
+                result = Math.Pow(FirstNumber, SecondNumber);
+
+            }
+
+                      
+            else
+
+            {
+                Console.WriteLine("Please enter a valid operation type");
+            }
+            Console.WriteLine($"Your result is: {Math.Abs(result)}");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+                                                  
