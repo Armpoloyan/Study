2e8e5636906235ab9c31e97d50921d6cd8aebec0
Arman <Arman@ARMAN-PC>
2018-10-03 17:00:24 +0400

    ddd

---
+++ UpperLettre/UpperLettre/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpperLettre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String upperLetrre;
+            Console.WriteLine("please enter lattre   ");
+
+            upperLetrre = Console.ReadLine();
+            Console.WriteLine(upperLetrre.ToUpper());
+            Console.ReadKey();
+        }
+    }
+}
