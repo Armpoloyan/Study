e23839caa8a72c15c69ed089011883617265f0d8
Arman <Arman@ARMAN-PC>
2018-10-10 16:28:00 +0400

    tnayin  10.10.18

---
+++ Homework-6-10.10.18/Homework-6-10.10.18/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6_10._10._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("greq shexankyan koxmi erkerutyun@  ");
+           int a =int.Parse(Console.ReadLine());
+                           
+           int b = 2*a;
+           int x = 0;               
+           for (int y = 0; y <= b/2; y++)
+            {
+                for (  ; x <= a; )
+                {
+                    Console.SetCursorPosition(5+a - x, 5+y);
+                    Console.Write('*');
+                    Console.SetCursorPosition(5+a + x, 5+y);
+                    Console.Write('*');
+                    x++;
+                    break;
+                }
+
+            }
+            int X = a;
+            for (int y = b/2; y <= b ; y++)
+            {
+                for ( ; X >= 0; )
+                {
+                    Console.SetCursorPosition(5+a- X, 5+y);
+                    Console.Write('*');
+                    Console.SetCursorPosition(5+a + X,5+ y);
+                    Console.Write('*');
+                    X--;
+                    break;
+                }
+
+            }
+                                
+            Console.ReadKey();
+            
+         }
+    }
+}
