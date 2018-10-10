e33e32648e1ba44dca0cdbb93f7102943b038b04
Arman <armpoloyan@gmail.com>
2018-10-10 17:01:09 +0400

    homwork

---
+++ EX 14-15/EX 14-15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_14_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Խնդիր_14:
+            // Ներածել n թիվը։ Արտածել yes, եթե այն հավասար է իր 
+            //թվանշանների գումարի կրկնապատիկին, no՝ հակառակ դեպքում։
+
+            Console.WriteLine("enter number N");
+            int N= int.Parse(Console.ReadLine());
+            int a = 0;
+            for (int i = N; i>=1; i--)
+            {
+                a
+            }
+        }
+    }
+}
---
+++ Shexankyun -ankyunagic/Shexankyun -ankyunagic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shexankyun__ankyunagic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("greq shexankyan poqr ankyunagic@  ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("greq shexankyan mec ankyunagic@  ");
+            int b = int.Parse(Console.ReadLine());
+
+            
+            int x = 0;
+            for (int y = 0; y <= b / 2; y++)
+            {
+                for (; x <= a;)
+                {
+                    Console.SetCursorPosition(5 + a - x, 5 + y);
+                    Console.Write('*');
+                    Console.SetCursorPosition(5 + a + x, 5 + y);
+                    Console.Write('*');
+                    x++;
+                    break;
+                }
+
+            }
+            int X = a;
+            for (int y = b / 2; y <= b; y++)
+            {
+                for (; X >= 0;)
+                {
+                    Console.SetCursorPosition(5 + a - X, 5 + y);
+                    Console.Write('*');
+                    Console.SetCursorPosition(5 + a + X, 5 + y);
+                    Console.Write('*');
+                    X--;
+                    break;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
