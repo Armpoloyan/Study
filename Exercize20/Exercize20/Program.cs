b7070729fc449dcedf8a197a0f05d8040949b4fd
Arman <armpoloyan@gmail.com>
2018-10-14 01:22:48 +0400

    homework

---
+++ Exercize20/Exercize20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercize20
+{
+    //Խնդիր_20:
+    // Գտնել տրված թվի ամենամեծ և ամենափոքր թվանշանների տարբերության քառակուսին։
+
+    class Program
+    {
+        static int DifferenceDigit(int number)
+        {
+            int difference = 0;
+            int max = 0, min = 9, t = 0;
+            for (; number > 0; number /= 10)
+            {
+                t = number % 10;
+                min = t < min ? t : min;
+                max = t > max ? t : max;
+                difference = max - min;
+            }
+            return difference;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter number  ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            int difference = DifferenceDigit(inputNumber);
+            Console.WriteLine($"(max - min)^2 = { difference* difference}.");
+            Console.ReadKey();
+        }
+    }
+}
