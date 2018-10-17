4faf5563461b03e1e356282e6cf4f3146f3067fa
Arman <armpoloyan@gmail.com>
2018-10-17 11:45:23 +0400

    homework

---
+++ Homework 10.17.18/Factorial/Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+            Console.ReadLine();
+        }
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+            else
+                return n *= Factorial(n - 1);
+        }
+    }
+}
---
+++ Homework 10.17.18/Fibonachi/Fibonachi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please enter Fibonachi nmber = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}-Fibonacci number is {1}", n,Fibonachi(n)); ;
+            Console.ReadKey();
+
+        }
+        static int Fibonachi( int n)
+        {
+            if (n <= 1)
+                return 1;
+            else return (Fibonachi(n - 1) + Fibonachi(n - 2));
+        }
+    }
+}
